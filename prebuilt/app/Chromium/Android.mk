66081c918ed7e58f24f2e96f3eb1dd6d6d096b7e
Menno van Grinsven <scanno71@gmail.com>
2018-06-25 20:51:01 +0200

    omni: Update Chromium for webview
    Change-Id: Ie2b09aab0fb09f25c10fa9756cc5b9a981c5b7b4

--- prebuilt/app/Chromium/Android.mk
+++ prebuilt/app/Chromium/Android.mk
@@ -5,8 +5,21 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := Chromium
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := APPS
-LOCAL_SRC_FILES    := 32bit/MonochromePublic.apk
-LOCAL_CERTIFICATE  := PRESIGNED
+#LOCAL_SRC_FILES    := 64bit/MonochromePublic.apk
+# LOCAL_CERTIFICATE  := PRESIGNED
 LOCAL_MODULE_PATH  := $(TARGET_OUT_APPS)
+LOCAL_MULTILIB := both
+LOCAL_CERTIFICATE := $(DEFAULT_SYSTEM_DEV_CERTIFICATE)
+LOCAL_REQUIRED_MODULES := \
+        libwebviewchromium_loader \
+        libwebviewchromium_plat_support
+
+LOCAL_MODULE_TARGET_ARCH := arm arm64
+my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
+LOCAL_SRC_FILES := $(my_src_arch)/MonochromePublic.apk
+
+LOCAL_PREBUILT_JNI_LIBS_arm := @lib/armeabi-v7a/libmonochrome.so
+LOCAL_PREBUILT_JNI_LIBS_arm64 := @lib/arm64-v8a/libmonochrome.so
+
 include $(BUILD_PREBUILT)
 endif
